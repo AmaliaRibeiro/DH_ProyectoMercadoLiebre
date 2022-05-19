b64bc5772e0008f4e2afccbc9f93f13ea216f1fa
Amalita9 <amalita9421@gmail.com>
2022-05-19 16:11:04 -0300

    feat: cambios en estilos, responsive, posicionamiento con flexbox

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express ();
 const publicPath = path.resolve(__dirname,"public")
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log('servidor corriendo'));
+app.listen(3030, () => console.log('servidor corriendo'));
 
-app.get ('/home', (req, res) => {
+app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/views/home.html'));
 });
